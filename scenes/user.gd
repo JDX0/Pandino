4bbed62a1fc5ba825396fe9957ee1cddb9310742
Jan Josefik <jdx@tutanota.de>
2023-01-18 14:48:17 +0100

    added verified testing account

--- scenes/user.gd
+++ scenes/user.gd
@@ -19,3 +19,6 @@ func _on_sign_up_button_pressed():
 	Database.sign_up(get_node("MarginContainer/VBoxContainer/EmailEdit").text,get_node("MarginContainer/VBoxContainer/PasswordEdit").text)
 	TransitionScene.transition("res://scenes/account_info.tscn")
 	
+func _on_testing_login_button_pressed():
+	Database.sign_in("dowagav865@fom8.com","password")
+	TransitionScene.transition("res://scenes/menu.tscn")
