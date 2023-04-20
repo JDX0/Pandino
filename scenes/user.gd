1bc0b3208ce66144cc9d9b694a9e120bd67083d0
Jan Josefik <jdx@tutanota.de>
2023-04-20 08:33:40 +0200

    Show auth errors and loading indicator on login screen

--- scenes/user.gd
+++ scenes/user.gd
@@ -2,31 +2,58 @@ extends Control
 
 var email_edit
 var password_edit
+var error_label
 
 func _ready():
 	email_edit = get_node("MarginContainer/PanelContainer/MarginContainer/VBoxContainer/EmailEdit")
 	password_edit = get_node("MarginContainer/PanelContainer/MarginContainer/VBoxContainer/PasswordEdit")
+	error_label = get_node("MarginContainer/PanelContainer/MarginContainer/VBoxContainer/ErrorLabel")
+	Supabase.auth.connect("signed_in", _on_signed_in)
+	Supabase.auth.connect("error", _on_auth_error)
 
 func _process(_delta):
 	pass
 	
 func sign_up(email,password):
+	error_label.visible = false
+	%Loading.visible = true
+	Supabase.auth.connect("signed_up", _on_signed_up)
 	var auth_task: AuthTask = await Supabase.auth.sign_up(
 		email,
 		password
 	).completed
-	State.user = auth_task.user
-	Database.insert_account_info("Unnamed Panda","Desc")
-	print(State.user)
-	TransitionScene.transition("res://scenes/account_info.tscn")
+	
 
 func _on_login_button_pressed():
-	Database.sign_in(email_edit.text,password_edit.text)
-	TransitionScene.transition("res://scenes/menu.tscn")
-
+	sign_in(email_edit.text,password_edit.text)
+	
 func _on_sign_up_button_pressed():
 	sign_up(email_edit.text,password_edit.text)
 	
 func _on_testing_login_button_pressed():
-	Database.sign_in("asecvqy669@tmail9.com","ciscocisco")
+	sign_in("asecvqy669@tmail9.com","ciscocisco")
+	
+func sign_in(email,password):
+	error_label.visible = false
+	%Loading.visible = true
+	Supabase.auth.sign_in(email,password)
+	
+func _on_signed_in(user : SupabaseUser):
+	%Loading.visible = false
+	State.user = user
+	print(State.user)
 	TransitionScene.transition("res://scenes/menu.tscn")
+	
+func _on_signed_up(user : SupabaseUser):
+	%Loading.visible = false
+	State.user = user
+	print(State.user)
+	Database.insert_account_info("Unnamed Panda","Desc")
+	TransitionScene.transition("res://scenes/account_info.tscn")
+	
+func _on_auth_error(error : SupabaseAuthError):
+	%Loading.visible = false
+	error_label.visible = true
+	error_label.text = error.hint
+	print(error.hint)
+
