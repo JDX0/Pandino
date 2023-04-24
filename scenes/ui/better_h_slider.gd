c89f2b33029891ae648142607eaf19bc9d9b6d3a
Jan Josefik <jdx@tutanota.de>
2023-04-24 16:48:19 +0200

    Animate Better H Sliders

--- scenes/ui/better_h_slider.gd
+++ scenes/ui/better_h_slider.gd
@@ -17,12 +17,18 @@ func _process(_delta):
 
 func _on_drag_started():
 	label.visible = true
+	$AnimationPlayer.play("show_label")
 
 
 func _on_drag_ended(_val_changed):
-	label.visible = false
+	$AnimationPlayer.play("hide_label")
 
 
 func _on_value_changed(val):
 	label.text = str(val)
 	label.position.x = (get_rect().size.x-grabber_width)*get_as_ratio()-0.5*label.get_rect().size.x+0.5*grabber_width
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "hide_label":
+		label.visible = false
