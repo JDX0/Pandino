ba2526ab5c88154e4d1b1001d082c3560d64d621
JDX <jdx@tutanota.de>
2023-05-08 22:53:08 +0200

    Better CheckButton and pause menu responsivity

--- scenes/ui/better_check_button.gd
+++ scenes/ui/better_check_button.gd
@@ -1,23 +1,15 @@
 extends Button
 		
-var stupid = false
-		
 func init(pressed):
 	button_pressed = pressed
 	if pressed:
-		stupid = true
 		$AnimationPlayer.play("RESET")
 	else:
 		$AnimationPlayer.play("RESET_REVERSE")
 
 func _on_toggled(button_pressed):
 	if button_pressed:
-		if stupid:
-			stupid = false
-			print("stupid")
-		else:
-			Sound.ui_forward()
-			print("not stupid")
+		Sound.ui_forward()
 		$AnimationPlayer.play("disable")
 	else:
 		Sound.ui_back()
